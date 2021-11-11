1a04b2daf9da708f2a76beac37bcc8711a23a7e6
Joy Fakhry <jfakhry@uci.edu>
2021-11-11 17:18:18 -0500

    test

--- test.py
+++ test.py
@@ -1,21 +1,24 @@
+import numpy as np
+
 from gym_2048.envs.game2048_env import Game2048Env
 from tqdm import trange
+import numpy
 
 env = Game2048Env()
 
 num_steps = 10_000
 
-## Testing merge
+rewards = np.ndarray(num_steps)
 
-while True:
+for t in trange(num_steps, desc='Steps'):
     a = env.action_space.sample()
     next_state, reward, done, info = env.step(a)
     env.render()
+    rewards[t] = reward
 
     if done:
         print(info)
-        break
+        env.reset()
 
-print(env.score)
-#ajshdbfkasbf
-#as;dfkhas;lkdfja;sdlkfj
+print(rewards)
+numpy.savetxt("rewards.csv", rewards, delimiter="   ")
