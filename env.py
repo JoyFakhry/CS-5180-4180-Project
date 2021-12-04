65e84db143c0a40761370e16dd356ae0d6db39ce
Joy Fakhry <jfakhry@uci.edu>
2021-12-04 17:13:05 -0500

    updated DQN, started distributional, added four rooms env

---
+++ env.py
@@ -0,0 +1,215 @@
+from enum import IntEnum
+from typing import Tuple, Optional, List
+from gym import Env, spaces, ObservationWrapper
+from gym.utils import seeding
+from gym.envs.registration import register
+from numpy import random
+
+
+def register_env() -> None:
+    """Register custom gym environment so that we can use `gym.make()`
+
+    In your main file, call this function before using `gym.make()` to use the Four Rooms environment.
+        register_env()
+        env = gym.make('FourRooms-v0')
+    """
+    register(id="FourRooms-v0", entry_point="env:FourRoomsEnv", max_episode_steps=500)
+
+
+class Action(IntEnum):
+    """Action"""
+
+    LEFT = 0
+    DOWN = 1
+    RIGHT = 2
+    UP = 3
+
+
+def actions_to_dxdy(action: Action) -> Tuple[int, int]:
+    """
+    Helper function to map action to changes in x and y coordinates
+    Args:
+        action (Action): taken action
+    Returns:
+        dxdy (Tuple[int, int]): Change in x and y coordinates
+    """
+    mapping = {
+        Action.LEFT: (-1, 0),
+        Action.DOWN: (0, -1),
+        Action.RIGHT: (1, 0),
+        Action.UP: (0, 1),
+    }
+    return mapping[action]
+
+
+class FourRoomsEnv(Env):
+    """Four Rooms gym environment."""
+
+    def __init__(self, goal_pos=(10, 10)) -> None:
+        self.rows = 11
+        self.cols = 11
+
+        # Coordinate system is (x, y) where x is the horizontal and y is the vertical direction
+        self.walls = [
+            (0, 5),
+            (2, 5),
+            (3, 5),
+            (4, 5),
+            (5, 0),
+            (5, 2),
+            (5, 3),
+            (5, 4),
+            (5, 5),
+            (5, 6),
+            (5, 7),
+            (5, 9),
+            (5, 10),
+            (6, 4),
+            (7, 4),
+            (9, 4),
+            (10, 4),
+        ]
+
+        self.start_pos = (0, 0)
+        self.goal_pos = goal_pos
+        self.agent_pos = None
+
+        self.action_space = spaces.Discrete(len(Action))
+        self.observation_space = spaces.Tuple(
+            (spaces.Discrete(self.rows), spaces.Discrete(self.cols))
+        )
+        # self.observation_space = spaces.Discrete(self.cols * self.rows)
+
+    def seed(self, seed: Optional[int] = None) -> List[int]:
+        """Fix seed of environment
+
+        In order to make the environment completely reproducible, call this function and seed the action space as well.
+            env = gym.make(...)
+            env.seed(seed)
+            env.action_space.seed(seed)
+
+        This function does not need to be used for this assignment, it is given only for reference.
+        """
+
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
+
+    def reset(self) -> Tuple[int, int]:
+        """Reset agent to the starting position.
+
+        Returns:
+            observation (Tuple[int,int]): returns the initial observation
+        """
+        self.agent_pos = self.start_pos
+
+        return self.agent_pos
+
+    def step(self, action: Action) -> Tuple[Tuple[int, int], float, bool, dict]:
+        """Take one step in the environment.
+
+        Takes in an action and returns the (next state, reward, done, info).
+        See https://github.com/openai/gym/blob/master/gym/core.py#L42-L58 foand r more info.
+
+        Args:
+            action (Action): an action provided by the agent
+
+        Returns:
+            observation (object): agent's observation after taking one step in environment (this would be the next state s')
+            reward (float) : reward for this transition
+            done (bool): whether the episode has ended, in which case further step() calls will return undefined results
+            info (dict): contains auxiliary diagnostic information (helpful for debugging, and sometimes learning). Not used in this assignment.
+        """
+
+        # Check if goal was reached
+        if self.agent_pos == self.goal_pos:
+            done = True
+            reward = 1.0
+        else:
+            done = False
+            reward = 0.0
+
+        # (there are 2 perpendicular actions for each action).
+        # You can reuse your code from ex0
+        n = self.noise()
+
+        if action == Action.UP:
+            if n == 1:
+                action_taken = Action.LEFT
+            elif n == 2:
+                action_taken = Action.RIGHT
+            else:
+                action_taken = Action.UP
+
+        if action == Action.LEFT:
+            if n == 1:
+                action_taken = Action.DOWN
+            elif n == 2:
+                action_taken = Action.UP
+            else:
+                action_taken = Action.LEFT
+
+        if action == Action.RIGHT:
+            if n == 1:
+                action_taken = Action.UP
+            elif n == 2:
+                action_taken = Action.DOWN
+            else:
+                action_taken = Action.RIGHT
+
+        if action == Action.DOWN:
+            if n == 1:
+                action_taken = Action.RIGHT
+            elif n == 2:
+                action_taken = Action.LEFT
+            else:
+                action_taken = Action.DOWN
+
+        # You can reuse your code from ex0
+        move = actions_to_dxdy(action_taken)
+        next_pos = (self.agent_pos[0] + move[0], self.agent_pos[1] + move[1])
+
+        # If the next position is a wall or out of bounds, stay at current position
+        # Set self.agent_pos
+        if next_pos in self.walls:  # If next state is a wall, keep current state
+            pass
+        elif (next_pos[0] < 0) or (next_pos[0] > 10):  # If x coordinate is out of bounds, keep current state
+            pass
+        elif (next_pos[1] < 0) or (next_pos[1] > 10):  # If y coordinate is out of bounds, keep current state
+            pass
+        else:
+            self.agent_pos = next_pos
+
+        return self.agent_pos, reward, done, {}
+
+    def noise(self):
+        r = random.rand()
+        if r <= 0.1:
+            return 1  # denotes perpendicular direction 1
+        elif r >= 0.9:
+            return 2  # denotes perpendicular direction 2
+        else:
+            return 0
+
+
+class FourRoomsWrapper1(ObservationWrapper):
+    """Wrapper for FourRoomsEnv to aggregate rows.
+
+    This wrapper aggregates rows, i.e. the original observation of the position (x,y) becomes just y
+
+    To use this wrapper, create the environment using gym.make() and wrap the environment with this class.
+
+    Ex) env = gym.make('FourRooms-v0')
+        env = FourRoomsWrapper1(env)
+
+    To use different function approximation schemes, create more wrappers, following this code.
+    """
+
+    def __init__(self, env):
+        super().__init__(env)
+        # Since this wrapper changes the observation space from a tuple of scalars to a single scalar,
+        # need to change the observation_space
+        self.observation_space = spaces.Discrete(self.cols * self.rows)
+
+    def observation(self, observation):
+        """Return the y-value of position"""
+        return observation[1]
--- test.py
+++ test.py
@@ -1,24 +1,223 @@
+import math, random
+
+import gym
 import numpy as np
 
-from gym_2048.envs.game2048_env import Game2048Env
-from tqdm import trange
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.autograd as autograd
+import torch.nn.functional as F
+from IPython.display import clear_output
+import matplotlib.pyplot as plt
+from tqdm import tqdm
 
-env = Game2048Env()
 
-num_steps = 10_000
+USE_CUDA = torch.cuda.is_available()
+Variable = lambda *args, **kwargs: autograd.Variable(*args, **kwargs).cuda() if USE_CUDA else autograd.Variable(*args, **kwargs)
 
-rewards = np.ndarray(num_steps)
-env.render()
-print(env.observation_space.shape)
+# Replay Buffer
+from collections import deque
 
-for t in trange(num_steps, desc='Steps'):
-    a = env.action_space.sample()
-    next_state, reward, done, info = env.step(a)
-    rewards[t] = reward
+class ReplayBuffer(object):
+    def __init__(self, capacity):
+        self.buffer = deque(maxlen=capacity)
 
-    if done:
-        env.reset()
-env.render()
+    def push(self, state, action, reward, next_state, done):
+        state = np.expand_dims(state, 0)
+        next_state = np.expand_dims(next_state, 0)
 
-print(rewards)
-np.savetxt("Data/rewards.csv", rewards, delimiter="   ")
+        self.buffer.append((state, action, reward, next_state, done))
+
+    def sample(self, batch_size):
+        state, action, reward, next_state, done = zip(*random.sample(self.buffer, batch_size))
+        return np.concatenate(state), action, reward, np.concatenate(next_state), done
+
+    def __len__(self):
+        return len(self.buffer)
+
+
+# Cart Pole Environment
+env_id = "CartPole-v0"
+env = gym.make(env_id)
+# from env import *
+# register_env()
+# env = gym.make('FourRooms-v0')
+# print(env.observation_space)
+
+# Epsilon greedy exploration
+epsilon_start = 1.0
+epsilon_final = 0.01
+epsilon_decay = 5000
+
+
+epsilon_by_frame = lambda frame_idx: epsilon_final + (epsilon_start - epsilon_final) * \
+                                     math.exp(-1. * frame_idx / epsilon_decay)
+
+# Deep Q Network
+
+
+class DQN(nn.Module):
+    def __init__(self, num_inputs, num_actions):
+        super(DQN, self).__init__()
+
+        self.layers = nn.Sequential(
+            nn.Linear(env.observation_space.shape[0], 128),
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, env.action_space.n)
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+    def act(self, state, epsilon):
+        if random.random() > epsilon:
+            with torch.no_grad():
+                state = Variable(torch.FloatTensor(state).unsqueeze(0))
+            q_value = self.forward(state)
+            action = q_value.max(1)[1].item()
+        else:
+            action = random.randrange(env.action_space.n)
+        return action
+
+
+# model = DQN(env.observation_space.shape[0], env.action_space.n)
+# optimizer = optim.Adam(model.parameters())
+
+# replay_buffer = ReplayBuffer(1000)
+
+# TD Loss
+def compute_td_loss(batch_size):
+    state, action, reward, next_state, done = replay_buffer.sample(batch_size)
+
+    state = Variable(torch.FloatTensor(np.float32(state)))
+    with torch.no_grad():
+        next_state = Variable(torch.FloatTensor(np.float32(next_state)))
+    action = Variable(torch.LongTensor(action))
+    reward = Variable(torch.FloatTensor(reward))
+    done = Variable(torch.FloatTensor(done))
+
+    q_values = model(state)
+    next_q_values = model(next_state)
+
+    q_value = q_values.gather(1, action.unsqueeze(1)).squeeze(1)
+    next_q_value = next_q_values.max(1)[0]
+    expected_q_value = reward + gamma * next_q_value * (1 - done)
+
+    loss = (q_value - Variable(expected_q_value.data)).pow(2).mean()
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    return loss
+
+
+def plot(rewards, losses):
+    clear_output(True)
+    plt.figure(figsize=(16, 8))
+    plt.subplot(121)
+    plt.title('frame %s. reward: %s' % (frame_idx, np.mean(rewards[-10:])))
+    plt.plot(rewards)
+    plt.xlabel('Episode')
+    plt.xlabel('Reward')
+    plt.subplot(122)
+    plt.title('loss')
+    plt.plot(losses)
+    plt.xlabel('Episode')
+    plt.xlabel('Loss')
+    plt.show()
+
+
+steps = 10000
+batch_size = 32
+gamma = 0.99
+
+
+def train(model):
+    losses = []
+    # all_rewards = []
+    all_rewards = np.zeros(EPISODES)
+    steps = np.zeros(EPISODES)
+    episode_reward = 0
+    step = 1
+    for i in tqdm(range(EPISODES)):
+
+        state = env.reset()
+        episode_reward = 0
+        while True:
+            epsilon = epsilon_by_frame(step)
+            action = model.act(state, epsilon)
+
+            next_state, reward, done, _ = env.step(action)
+            replay_buffer.push(state, action, reward, next_state, done)
+
+            state = next_state
+            episode_reward += reward
+
+            if done:
+                # state = env.reset()
+                # all_rewards.append(episode_reward)
+                all_rewards[i] = episode_reward
+                steps[i] = step
+                break
+
+            if len(replay_buffer) > batch_size:
+                loss = compute_td_loss(batch_size)
+                losses.append(loss.item())
+
+            step += 1
+
+    return all_rewards, losses, steps
+
+
+TRIALS = 3
+EPISODES = 100
+
+data = np.zeros((TRIALS, EPISODES))
+# rewards = np.zeros((TRIALS, EPISODES))
+step = np.zeros((TRIALS, EPISODES))
+for t in range(TRIALS):
+    model = DQN(env.observation_space.shape[0], env.action_space.n)
+    optimizer = optim.Adam(model.parameters())
+    replay_buffer = ReplayBuffer(1000)
+    rewards, _, step[t,:] = train(model)
+    data[t] = rewards
+    # rewards = np.array(rewards)
+
+
+plt.figure(figsize=(16, 8))
+plt.subplot(121)
+# plt.title('frame %s. reward: %s' % (frame_idx, np.mean(rewards[-10:])))
+std = step.std(axis=0)
+avg = step.mean(axis=0)
+length = len(std)
+y_err = 1.96 * std * np.sqrt(1 / length)
+plt.fill_between(np.linspace(0, length - 1, length), avg - y_err, avg + y_err, alpha=0.2)
+
+plt.plot(step.mean(axis=0))
+# plt.xlabel('Episode')
+# plt.xlabel('Reward')
+plt.subplot(122)
+# plt.title('loss')
+# plt.plot(losses)
+# plt.xlabel('Episode')
+# plt.xlabel('Loss')
+# plt.show()
+
+avg = data.mean(axis=0)
+std = data.std(axis=0)
+length = len(avg)
+y_err = 1.96 * std * np.sqrt(1 / length)
+plt.fill_between(np.linspace(0, length - 1, length), avg - y_err, avg + y_err, alpha=0.2)
+
+plt.plot(avg, label='DQN')
+plt.xlabel("Episodes")
+plt.ylabel("Rewards")
+plt.legend() #loc=3, fontsize='small')
+plt.title(f'Cartpole v0 Average Rewards over {TRIALS} runs')
+
+plt.show()
+
